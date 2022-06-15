b53f2a5680c1afff58bba50b25c6aa9eab47ee1f
Artem <nosov.a.o@yandex.ru>
2022-06-15 21:43:16 +0300

    gRPC - impl SERVER #1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static void main( String[] args ) throws IOException, InterruptedException {
+        Server server = ServerBuilder
+                .forPort(8080)
+                .addService(new GreetingServiceImpl())
+                .build();
+        server.start();
+
+
+        System.out.printf("Server started");
+
+        server.awaitTermination();
+    }
+}
